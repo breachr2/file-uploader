1ceee3b6298fe73f57e6ea6f3b86751dbcc351e4
breachr <brianhoang873@gmail.com>
2025-02-12 15:17:28 -0800

    lint fix

--- client/src/hooks/useCreateFolder.ts
+++ client/src/hooks/useCreateFolder.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { createFolder } from "@/api/folder-api";
 
 const useCreateFolder = () => {
