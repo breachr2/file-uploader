b094dc3da929ebc770b5f1da21fce8f5751136a3
breachr <brianhoang873@gmail.com>
2025-02-14 22:23:33 -0800

    Redeploy

--- client/src/pages/auth/sign-up.tsx
+++ client/src/pages/auth/sign-up.tsx
@@ -6,7 +6,6 @@ import {
   CardTitle,
   CardFooter,
 } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
